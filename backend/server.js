fbe874b88db88e8af830c0a612e225b395d9f936
Rabi Bhagat <rabibhagat789@gmail.com>
2026-02-25 19:11:56 +0530

    Fix Express 5 wildcard bug

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, '..', 'frontend', 'dist', 'index.html'));
     });
 }
+// 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
