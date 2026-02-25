0be15bf5ec77f5f0bbb7e30ea2adbdfb0966b54e
Rabi Bhagat <rabibhagat789@gmail.com>
2026-02-25 18:59:49 +0530

    crash fix

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use('/api/todos', require('./routes/todos'));
 // Serve static assets in production (if applicable)
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('../frontend/dist'));
-    app.get('*', (req, res) => {
+    app.use((req, res) => {
         res.sendFile(path.resolve(__dirname, '..', 'frontend', 'dist', 'index.html'));
     });
 }
